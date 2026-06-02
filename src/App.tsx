29d0f0d1f9d7074cc56fd247d07016fc0c280975
Sven Wehrend <swehrend@ant-neuro.com>
2026-06-02 19:21:04 +0200

    fix configuration and add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1 className="text-4xl font-bold text-blue-500">Hello</h1>;
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">Hello</h1>
+      <Button>TEST</Button>
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // ← add this
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // ← add this
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
